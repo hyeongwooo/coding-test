f71a7fe35de8b8fbb842b846564f124f6ba2ac3b
주형우 <wnguddn777@juhyeong-uui-MacBookAir.local>
2024-01-17 20:17:59 +0900

    second commit

---
+++ overwrite.py
@@ -0,0 +1,20 @@
+# my_string, overwrite_string, s_str = input().strip().split(' ')
+# s = input(s_str)
+# def solution(my_string, overwrite_string, s):
+#     answer = my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
+#     return answer
+
+# result = solution(my_string, overwrite_string, s)
+# print(result)
+
+def solution(my_string, overwrite_string, s):
+    answer = my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
+    return answer
+
+# 입력 받기
+my_string, overwrite_string, s_str = input().strip().split(' ')
+s = int(s_str)
+
+# 함수 호출 및 결과 출력
+result = solution(my_string, overwrite_string, s)
+print(result)
